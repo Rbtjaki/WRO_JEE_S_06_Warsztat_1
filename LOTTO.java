06edb9b99782f3ac37b8a65bbe6dc5d0adef6895
Rbtjaki <42670508+Rbtjaki@users.noreply.github.com>
2018-10-14 20:42:06 +0300

    Update LOTTO.java

--- LOTTO.java
+++ LOTTO.java
@@ -13,25 +13,49 @@ public class LOTTO {
         System.out.println("Podaj 6 liczb z zakresu 1 - 49, bo tylko takie moga wygrac: ");
 
         final int z = 49;
-        int[] user = new int[6];
-        int[] lotto = new int[6];
+
+
         int[] zakres = new int[z];
         int num = 0;
         int n = 0;
+        int[] nowa = new int[6];
+        int licznik = 0;
 
         for (int i = 0; i < z; i++) {
             zakres[i] = n++;
         }
-
+        int[] user = new int[6];
         for (int j = 0; j < 6; j++) {
             user[j] = skan.nextInt();
-                for (int k = 0; k < j; k++){
-                   while (user[j] == user[k]){
-                        System.out.println("Liczba sie powtorzyla, musisz wybrac inna: ");
-                        user[k] = skan.nextInt();
-                    }
+            for (int k = 0; k < j; k++){
+                while (user[j] == user[k]){
+                    System.out.println("Liczba sie powtorzyla, musisz wybrac inna: ");
+                    user[k] = skan.nextInt();
                 }
+                while (user[k] > 49){
+                    System.out.println("Liczba jest za duza, musisz wybrac inna: ");
+                    user[k] = skan.nextInt();
+                }
+                while (user[k] < 0){
+                    System.out.println("Liczba jest za mala, musisz wybrac inna: ");
+                    user[k] = skan.nextInt();
+                }
+            }
         }
+        int bufor = user[0];
+
+        for (int i = 0; i < user.length; i++){
+            bufor = user[0];
+            for (int j = 0; j < user.length; j++){
+                if (bufor > user[j]){
+                    bufor = user[j];
+                    licznik = j;
+                }
+            }nowa[i] = bufor;
+            user[licznik] = 51;
+            System.out.print(" " + nowa[i]);
+        }
+        int[] lotto = new int[6];
         for (int i = 0; i < 6; i++) {
             int x = los.nextInt(49+1)+1;
             for (int j = 0; j < 49; j++) {
@@ -39,8 +63,7 @@ public class LOTTO {
                     zakres[i] = -1;
                 }
             }lotto[i] = x;
-
-            System.out.print(lotto[i] + " ");
+            System.out.println(lotto[i] + " ");
         }
         int check = 0;
         for (int i = 0; i < 6; i++) {
